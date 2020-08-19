5e9e9b04d872ce09ecb52af4fa79dc5b65b92038
Iván Pérez <ivanperezfernandezsuper@gmail.com>
2020-08-20 00:55:34 +0200

    Add function to interact with api server
    Not functional yet, and needs heavy testing, but it's a good first impression

--- discord/js/js.js
+++ discord/js/js.js
@@ -1,12 +1,21 @@
-function copyandgo() {
-    var copyText = document.getElementById("userid");
-    var copyNumber = document.getElementById("numberid");
-    var textArea = document.createElement("textarea");
-    textArea.value = copyText.textContent + copyNumber.textContent;
-    document.body.appendChild(textArea);
-    textArea.select();
-    textArea.setSelectionRange(0, 99999); /*For mobile devices*/
+function CopyAndGo() {
+    var CopyText = document.getElementById("userid");
+    var CopyNumber = document.getElementById("numberid");
+    var TextArea = document.createElement("textarea");
+    TextArea.value = CopyText.TextContent + CopyNumber.TextContent;
+    document.body.appendChild(TextArea);
+    TextArea.select();
+    TextArea.setSelectionRange(0, 99999); /*For mobile devices*/
     document.execCommand("Copy");
-    textArea.remove();
-    window.location.href='https://discordapp.com/channels/@me/';
+    TextArea.remove();
+    window.location.href = 'https://discordapp.com/channels/@me/';
+}
+
+async function getUserId() {
+    let params = new URLSearchParams(location.search);
+    let response = await fetch('https://tracr.co/api/users/1/' + params.get('id'));
+    let data = await response.json()
+    document.getElementById("userid").innerHTML = data["username"];
+    document.getElementById("numberid").innerHTML = data["discriminator"];
+    document.getElementById("urlImage").style = "background-image: url(https://cdn.discordapp.com/avatars/" + data["id"] + "/" + data["avatar_url"] + ".webp?size=128);"
 }
