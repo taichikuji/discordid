e4649912ae0f9e1b918069786408364905b0a3a3
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2022-05-06 10:45:31 +0000

    Add initial redirection towards the Desktoop App

--- dist/js/js.js
+++ dist/js/js.js
@@ -1,3 +1,19 @@
+async function getData() {
+    let params = new URLSearchParams(location.search);
+    if (!isNaN(params.get('id')) && parseInt(params.get('id'))) {
+        window.location.href = 'https://discord.com/users/' + params.get('id');
+        const response = await fetch("./.netlify/functions/node-fetch?id=" + params.get('id')).then((resp) => resp.json());
+        const data = response['msg'];
+        document.getElementById("DiscTitle").innerHTML += " · " + data.username;
+        document.getElementById("UserId").innerHTML = data.username;
+        document.getElementById("NumberId").innerHTML = "#" + data.discriminator;
+        document.getElementById("AccessBtn").innerHTML = "Add " + data.username;
+        document.getElementById("UrlImage").style = "background-image: url(https://cdn.discordapp.com/avatars/" + data.id + "/" + data.avatar + ".webp);";
+    } else {
+        document.getElementById("Square").innerHTML = "<p>Hello! I'm Taichi.<br/>You're visiting the site without an ID, please visit the wiki for more info <a href='https://github.com/taichikuji/discordid/wiki'>「here」</a></p>";
+    }
+}
+
 function CopyAndGo() {
     let CopyText = document.getElementById("UserId");
     let CopyNumber = document.getElementById("NumberId");
@@ -10,19 +26,4 @@ function CopyAndGo() {
     TextArea.remove();
     let params = new URLSearchParams(location.search);
     window.location.href = 'https://discord.com/users/' + params.get('id');
-}
-
-async function getData() {
-    let params = new URLSearchParams(location.search);
-    if (!isNaN(params.get('id')) && parseInt(params.get('id'))) {
-        const response = await fetch("./.netlify/functions/node-fetch?id=" + params.get('id')).then((resp) => resp.json());
-        const data = response['msg'];
-        document.getElementById("DiscTitle").innerHTML += " · " + data.username;
-        document.getElementById("UserId").innerHTML = data.username;
-        document.getElementById("NumberId").innerHTML = "#" + data.discriminator;
-        document.getElementById("AccessBtn").innerHTML = "Add " + data.username;
-        document.getElementById("UrlImage").style = "background-image: url(https://cdn.discordapp.com/avatars/" + data.id + "/" + data.avatar + ".webp);";
-    } else {
-        document.getElementById("Square").innerHTML = "<p>Hello! I'm Taichi.<br/>You're visiting the site without an ID, please visit the wiki for more info <a href='https://github.com/taichikuji/discordid/wiki'>「here」</a></p>";
-    }
 }
