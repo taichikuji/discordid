1a40c180035993f89a0108ed1f6b2481f3008c5b
Iván Pérez <48720361+taichikuji@users.noreply.github.com>
2022-05-07 12:26:16 +0000

    Add back timeout if discord desktop does not trigger

--- dist/js/js.js
+++ dist/js/js.js
@@ -1,43 +1,29 @@
-function CopyAndGo() {
-    let CopyText = document.getElementById("UserId");
-    let CopyNumber = document.getElementById("NumberId");
-    let TextArea = document.createElement("TextArea");
-    TextArea.value = CopyText.textContent + CopyNumber.textContent;
-    document.body.appendChild(TextArea);
-    TextArea.select();
-    TextArea.setSelectionRange(0, 99999); /*For mobile devices*/
-    navigator.clipboard.writeText(TextArea.value); /* Copy to Clipboard using Clipbard API */
-    TextArea.remove();
-
+function Redirect() {
     const params = new URLSearchParams(location.search);
     const userId = params.get("id");
-
     if (isMobileDevice()) {
         window.location.href = "https://discord.com/users/" + userId;
     } else {
         window.location.href = "discord://-/users/" + userId;
+        setTimeout(() => {
+            if(document.hasFocus()) {
+                window.location.href = 'https://discord.com/users/' + userId;
+            }
+        }, 25)
     }
 }
 
 async function getData() {
     const params = new URLSearchParams(location.search);
     const userId = params.get("id");
-
     if (!isNaN(userId) && parseInt(userId)) {
-        const response = await fetch(
-            "./.netlify/functions/node-fetch?id=" + userId
-        ).then((resp) => resp.json());
+        const response = await fetch("./.netlify/functions/node-fetch?id=" + userId).then((resp) => resp.json());
         const data = response["msg"];
         document.getElementById("DiscTitle").innerHTML += " · " + data.username;
         document.getElementById("UserId").innerHTML = data.username;
         document.getElementById("NumberId").innerHTML = "#" + data.discriminator;
         document.getElementById("AccessBtn").innerHTML = "Add " + data.username;
-        document.getElementById("UrlImage").style =
-            "background-image: url(https://cdn.discordapp.com/avatars/" +
-            data.id +
-            "/" +
-            data.avatar +
-            ".webp);";
+        document.getElementById("UrlImage").style = "background-image: url(https://cdn.discordapp.com/avatars/" + data.id + "/" + data.avatar + ".webp);";
     } else {
         document.getElementById("Square").innerHTML =
             "<p>Hello! I'm Taichi.<br/>You're visiting the site without an ID, please visit the wiki for more info <a href='https://github.com/taichikuji/discordid/wiki'>「here」</a></p>";
@@ -49,11 +35,9 @@ function isMobileDevice() {
     if (window.navigator.userAgentData) {
         return window.navigator.userAgentData.mobile;
     }
-
     // Fallback for devices that don't support client hints
     if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
         return true;
     }
-
     return false;
 }
