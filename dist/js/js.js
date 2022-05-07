0cf07d8338766f813ba536f1ccc86ce6a83d4c21
Jannis Becker <jbecker@jannisbecker.me>
2022-05-07 11:13:17 +0200

    Add mobile device check

--- dist/js/js.js
+++ dist/js/js.js
@@ -8,27 +8,52 @@ function CopyAndGo() {
     TextArea.setSelectionRange(0, 99999); /*For mobile devices*/
     navigator.clipboard.writeText(TextArea.value); /* Copy to Clipboard using Clipbard API */
     TextArea.remove();
-    let params = new URLSearchParams(location.search);
 
-    window.location.href = 'discord://-/users/' + params.get('id');
-    setTimeout(() => {
-        if(document.hasFocus()) {
-            window.location.href = 'https://discord.com/users/' + params.get('id');
-        }
-    }, 300)
+    const params = new URLSearchParams(location.search);
+    const userId = params.get("id");
+
+    if (isMobileDevice()) {
+        window.location.href = "https://discord.com/users/" + userId;
+    } else {
+        window.location.href = "discord://-/users/" + userId;
+    }
 }
 
 async function getData() {
-    let params = new URLSearchParams(location.search);
-    if (!isNaN(params.get('id')) && parseInt(params.get('id'))) {
-        const response = await fetch("./.netlify/functions/node-fetch?id=" + params.get('id')).then((resp) => resp.json());
-        const data = response['msg'];
+    const params = new URLSearchParams(location.search);
+    const userId = params.get("id");
+
+    if (!isNaN(userId) && parseInt(userId)) {
+        const response = await fetch(
+            "./.netlify/functions/node-fetch?id=" + userId
+        ).then((resp) => resp.json());
+        const data = response["msg"];
         document.getElementById("DiscTitle").innerHTML += " · " + data.username;
         document.getElementById("UserId").innerHTML = data.username;
         document.getElementById("NumberId").innerHTML = "#" + data.discriminator;
         document.getElementById("AccessBtn").innerHTML = "Add " + data.username;
-        document.getElementById("UrlImage").style = "background-image: url(https://cdn.discordapp.com/avatars/" + data.id + "/" + data.avatar + ".webp);";
+        document.getElementById("UrlImage").style =
+            "background-image: url(https://cdn.discordapp.com/avatars/" +
+            data.id +
+            "/" +
+            data.avatar +
+            ".webp);";
     } else {
-        document.getElementById("Square").innerHTML = "<p>Hello! I'm Taichi.<br/>You're visiting the site without an ID, please visit the wiki for more info <a href='https://github.com/taichikuji/discordid/wiki'>「here」</a></p>";
+        document.getElementById("Square").innerHTML =
+            "<p>Hello! I'm Taichi.<br/>You're visiting the site without an ID, please visit the wiki for more info <a href='https://github.com/taichikuji/discordid/wiki'>「here」</a></p>";
     }
+}
+
+function isMobileDevice() {
+    // Check in client hints if device is mobile
+    if (window.navigator.userAgentData) {
+        return window.navigator.userAgentData.mobile;
+    }
+
+    // Fallback for devices that don't support client hints
+    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+        return true;
+    }
+
+    return false;
 }
